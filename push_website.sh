21e04c3ffe76c75895716c9b4f3ba3a234062026
Mary Southern <marys@cs.umn.edu>
2021-06-17 18:57:54 -0500

    Making the script push_website.sh executable in preparation for the commit hook, and fixing a typo in the script.

--- push_website.sh
+++ push_website.sh
@@ -7,5 +7,5 @@ if [ -z $WEBSITE_URL ]
 then
     echo -e "Please define an url in the environment variable 'WEBSITE_URL' for using the scp command\nAs an example, 'export WEBSITE_URL=south@cs-pancham.cs.umn.edu' set the url to 'south@cs-pancham.cs.umn.edu'"
 else
-    rsync -arzv -p --chmod=ug+rwX,o+rX -- exclude 'Makefile' --exclude 'examples/Makefile' --exclude 'examples/*/Makefile' --include='*.html' --include='*.thm' --include='*.lf' --include='*.css' --include='*.tar' --include='*/' --exclude='*' --prune-empty-dirs . $WEBSITE_URL:$WEBSITE_REL_PATH
+    rsync -arzv -p --chmod=ug+rwX,o+rX --exclude 'Makefile' --exclude 'examples/Makefile' --exclude 'examples/*/Makefile' --include='*.html' --include='*.thm' --include='*.lf' --include='*.css' --include='*.tar' --include='*/' --exclude='*' --prune-empty-dirs . $WEBSITE_URL:$WEBSITE_REL_PATH
 fi
