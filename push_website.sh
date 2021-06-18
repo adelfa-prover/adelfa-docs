149ab391c9c9ce8f9b18ad20a46a15d0bfd0857f
Mary Southern <marys@cs.umn.edu>
2021-06-17 19:05:38 -0500

    Testing post-commit hook

--- push_website.sh
+++ push_website.sh
@@ -2,7 +2,6 @@
 
 WEBSITE_REL_PATH=/web/research/sparrow.cs.umn.edu/adelfa
 
-
 if [ -z $WEBSITE_URL ] 
 then
     echo -e "Please define an url in the environment variable 'WEBSITE_URL' for using the scp command\nAs an example, 'export WEBSITE_URL=south@cs-pancham.cs.umn.edu' set the url to 'south@cs-pancham.cs.umn.edu'"
