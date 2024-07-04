02838103754c6b754fab38e4caa1d3c69d481eaf
Chase Johnson <cjohnson19@pm.me>
2024-07-03 23:52:16 -0500

    Reference guide pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   latex: true,
   themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
 });
 
 module.exports = withNextra();
