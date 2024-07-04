572e7ffffc3d4c3badc291c5c146b94159d92f02
Chase Johnson <cjohnson19@pm.me>
2024-07-03 22:50:18 -0500

    Initial commit from template

---
+++ next.config.js
@@ -0,0 +1,7 @@
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  latex: true,
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra();
---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Adelfa</span>,
+  project: {
+    link: "https://github.com/cjohnson19/nextra-adelfa-docs",
+  },
+  docsRepositoryBase: "https://github.com/cjohnson19/nextra-adelfa-docs",
+};
+
+export default config;
