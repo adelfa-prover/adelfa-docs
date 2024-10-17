e7886d7f71ce7d75e345b90dde808a1398bc05ed
Chase Johnson <cjohnson19@pm.me>
2024-10-17 11:21:54 -0500

    Fix links to repository

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,10 +5,10 @@ import { Logo } from "./components/logo";
 const config: DocsThemeConfig = {
   logo: <Logo />,
   project: {
-    link: "https://github.com/cjohnson19/nextra-adelfa-docs",
+    link: "https://github.com/adelfa-prover/adelfa-docs",
   },
   docsRepositoryBase:
-    "https://github.com/cjohnson19/nextra-adelfa-docs/tree/main",
+    "https://github.com/adelfa-prover/adelfa-docs/tree/main",
   head: () => {
     const { frontMatter } = useConfig();
 
