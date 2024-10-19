9ba65da35030a160ccd570712e1fd2c8851b8a0d
Chase Johnson <cjohnson19@pm.me>
2024-10-18 23:25:46 -0500

    Add SST files for deployment

---
+++ sst.config.ts
@@ -0,0 +1,35 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "adelfa-docs",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("AdelfaDocs");
+  },
+  console: {
+    autodeploy: {
+      target(event) {
+        // Use the `main` branch as the trigger for production deployment.
+        if (
+          event.type === "branch" &&
+          event.branch === "main" &&
+          event.action === "pushed"
+        ) {
+          return {
+            stage: "production",
+          };
+        }
+        if (event.type === "pull_request") {
+          return {
+            stage: `pr-${event.number}`,
+          };
+        }
+      },
+    },
+  },
+});
