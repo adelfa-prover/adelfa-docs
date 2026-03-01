5e69e076c481daf344bef22bd1bc8d0c4bb07822
Chase Johnson <cjohnson19@pm.me>
2026-02-28 18:34:58 -0600

    fix: remove frontmatter from search

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,22 +1,38 @@
 import { source } from "@/lib/source";
-import { createFromSource } from "fumadocs-core/search/server";
+import { createSearchAPI } from "fumadocs-core/search/server";
 import { structure } from "fumadocs-core/mdx-plugins";
+import { getBreadcrumbItems } from "fumadocs-core/breadcrumb";
 
 export const revalidate = false;
 
-export const { staticGET: GET } = createFromSource(source, {
-  buildIndex: async (page) => {
-    let structuredData = page.data.structuredData;
-    if (!structuredData) {
-      const raw = await page.data.getText("raw");
-      structuredData = structure(raw);
-    }
-    return {
-      id: page.url,
-      title: page.data.title,
-      description: page.data.description,
-      url: page.url,
-      structuredData,
-    };
-  },
+export const { staticGET: GET } = createSearchAPI("advanced", {
+  indexes: () =>
+    Promise.all(
+      source.getPages().map(async (page) => {
+        const breadcrumbs = getBreadcrumbItems(page.url, source.pageTree, {
+          includePage: true,
+        })
+          .map((v) => v.name?.toString())
+          .filter((v) => v !== undefined);
+        let structuredData = page.data.structuredData;
+        if (!structuredData) {
+          const raw = await page.data.getText("raw");
+          // the raw content includes the frontmatter of the page so we trim it
+          // out here. One may expect that we can use the processed text instead
+          // of raw, but the processed text includes extra content from
+          // remark plugins. For example, `## Header` will be processed into
+          // markdown with an id slug `Header [#header]`.
+          const content = raw.replace(/^---[\s\S]*?---\n*/, "");
+          structuredData = structure(content);
+        }
+        return {
+          id: page.url,
+          title: page.data.title,
+          description: page.data.description,
+          url: page.url,
+          breadcrumbs: breadcrumbs.length > 1 ? breadcrumbs : [],
+          structuredData,
+        };
+      }),
+    ),
 });
